f2182db1276025953e61bdea44eccb2af6e26cc3
jun <dltkd1214@naver.com>
2019-03-26 10:06:10 +0900

    버그 수정

--- src/main/java/com/ddoriya/sigar/utill/SigarUtil.java
+++ src/main/java/com/ddoriya/sigar/utill/SigarUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************** */
-package com.ddoriya.sigar.utill
+package com.ddoriya.sigar.utill;
 
 import java.io.File;
 import java.io.FileOutputStream;
